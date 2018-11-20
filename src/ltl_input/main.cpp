4190df40afb8b2da2bb95762bca317efbd2e5548
poposhca <jjphed@outlook.com>
2018-11-19 22:22:59 -0600

    Add LTL input node and some code for startup.

---
+++ src/ltl_input/main.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <string>
+#include <spot/tl/parse.hh>
+#include <spot/tl/print.hh>
+#include <spot/twaalgos/translate.hh>
+
+#define RATE_HZ 5
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ltl_input");
+    ros::NodeHandle nh;
+    ros::Rate loop_rate(RATE_HZ);
+
+    string ltl_formula = "!F(red & X(yellow))";
+    spot::formula pf = spot::parse_formula(ltl_formula);
+    print_lbt_ltl(std::cout, pf) << endl;
+    return 0;
+}
