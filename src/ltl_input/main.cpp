db70d8d5d50a0efba5b6cf12faed89db99e25e3f
poposhca <jjphed@outlook.com>
2019-01-14 22:07:17 -0600

    Correct minor cmake bug.

--- src/ltl_input/main.cpp
+++ src/ltl_input/main.cpp
@@ -1,8 +1,9 @@
 #include <ros/ros.h>
 #include <iostream>
 #include <string>
-// #include <spot/tl/parse.hh>
-// #include <spot/tl/print.hh>
+#include <spot/tl/parse.hh>
+#include <spot/twaalgos/translate.hh>
+#include <spot/twaalgos/hoa.hh>
 
 #define RATE_HZ 5
 
@@ -12,10 +13,14 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "ltl_input");
     ros::NodeHandle nh;
-    ros::Rate loop_rate(RATE_HZ);
-
-    // string ltl_formula = "!F(red & X(yellow))";
-    // spot::formula pf = spot::parse_formula(ltl_formula);
-    // print_lbt_ltl(std::cout, pf) << endl;
+    //ros::Rate loop_rate(RATE_HZ);
+    string ltl_formula = "!F(red & X(yellow))";
+    spot::parsed_formula pf = spot::parse_infix_psl(ltl_formula);
+    spot::translator trans;
+    trans.set_type(spot::postprocessor::Monitor);
+    trans.set_pref(spot::postprocessor::Deterministic);
+    spot::twa_graph_ptr aut = trans.run(pf.f);
+    print_hoa(std::cout, aut) << '\n';
+    cout << "Just testing the node" << endl;
     return 0;
 }
